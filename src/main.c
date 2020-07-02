67532fd6c51c4f2de3b4f7382dad738fbeaafbfb
Eugene Lee <eugenehlee99@gmail.com>
2020-07-01 19:58:42 -0700

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
+/******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "compass_sim.h"
 #include "nmea.h"
 #include <stddef.h>
 #include <string.h>
 #include <stdio.h>
-extern NMEA_GGA_t GGA;
+/******************************************************************************
+ * Defines
+ ******************************************************************************/
 
+/******************************************************************************
+ * Typedefs
+ ******************************************************************************/
+
+/******************************************************************************
+ * Global Data
+ ******************************************************************************/
+
+/******************************************************************************
+ * Module Static Data
+ ******************************************************************************/
+static int testCompass(void);
+/******************************************************************************
+ * Local Function Prototypes
+ ******************************************************************************/
+
+/******************************************************************************
+ * Function Definitions
+ ******************************************************************************/
 int main(int argc, char const *argv[])
 {
     NMEA_Config_t nmeaConfig;
@@ -13,68 +38,79 @@ int main(int argc, char const *argv[])
 
     NMEA_Init(&nmeaConfig);
 
-    //const char* nmeaString = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47";
-    //const char* nmeaString = "$GPGLL,4916.45,N,12311.12,W,225444,A,*1D";
-    const char* nmeaString = "$GPZDA,201530.00,04,07,2002,00,00*60";
-    /*format: $--GGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*/
+    const char* nmeaString = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47";
+    //const char* nmeaString = "$GPGLL,4916.45,N,12311.12,W,225444,A,*1D";  //!< GLL string example
+    //const char* nmeaString = "$GPZDA,201530.00,04,07,2002,00,00*60";      //!< ZDA string example
 
     for(i = 0; i < strlen(nmeaString); i++)
     {
         switch(NMEA_Decode(nmeaString[i]))
         {
             case NMEA_MESSAGE_NONE:
-                {
                     continue;
-                }
             case NMEA_MESSAGE_GGA:
-                {
-                    printf("talkerID = %c\n", NMEA_Data.GGA.talkerID);
-                    printf("fixHour = %d\n", NMEA_Data.GGA.fixTime[0]);
-                    printf("fixMinute = %d\n", NMEA_Data.GGA.fixTime[1]);
-                    printf("fixSecond = %d\n", NMEA_Data.GGA.fixTime[2]);
-                    printf("latitude = %f\n", NMEA_Data.GGA.latitude);
-                    printf("longitude = %f\n", NMEA_Data.GGA.longitude);
-                    printf("fixQuality = %d\n", NMEA_Data.GGA.fixQuality);
-                    printf("nSatellites = %d\n", NMEA_Data.GGA.nSatellites);
-                    printf("hdop = %f\n", NMEA_Data.GGA.hdop);
-                    printf("altitude = %f\n", NMEA_Data.GGA.altitude);
-                    printf("elevation = %f\n", NMEA_Data.GGA.elevation);
-                    printf("dGpsStale = %d\n", NMEA_Data.GGA.dGpsStale);
-                    printf("dGpsID = %d\n", NMEA_Data.GGA.dGpsID);
-                }
+                // deal with the data
                 break;
             case NMEA_MESSAGE_GLL:
-                {
-                    printf("talkerID = %c\n", NMEA_Data.GLL.talkerID);
-                    printf("latitude = %f\n", NMEA_Data.GLL.latitude);
-                    printf("longitude = %f\n", NMEA_Data.GLL.longitude);
-                    printf("fixHour = %d\n", NMEA_Data.GLL.fixTime[0]);
-                    printf("fixMinute = %d\n", NMEA_Data.GLL.fixTime[1]);
-                    printf("fixSecond = %d\n", NMEA_Data.GLL.fixTime[2]);
-                    printf("fixType = %c\n", NMEA_Data.GLL.fixType);
-                }
+                // deal with the data
                 break;
             case NMEA_MESSAGE_ZDA:
-                {
-                    printf("talkerID = %c\n", NMEA_Data.ZDA.talkerID);
-                    printf("fixHour = %d\n", NMEA_Data.ZDA.fixTime[0]);
-                    printf("fixMinute = %d\n", NMEA_Data.ZDA.fixTime[1]);
-                    printf("fixSecond = %d\n", NMEA_Data.ZDA.fixTime[2]);
-                    printf("day = %d\n", NMEA_Data.ZDA.day);
-                    printf("month = %d\n", NMEA_Data.ZDA.month);
-                    printf("year = %d\n", NMEA_Data.ZDA.year);
-                    printf("Zone Hours = %d\n", NMEA_Data.ZDA.zoneHours);
-                    printf("Zone Minutes = %d\n", NMEA_Data.ZDA.zoneMinutes);
-                }
+                // deal with the data
                 break;
             case NMEA_MESSAGE_ERROR:
                 {
-                    printf("error\n");
                     return 1;
                 }
             default:
                 break;
         }
     }
+
+    if(!testCompass())
+    {
+        printf("testCompass failed\n");
+    }
+
     return 0;
+}
+
+/**
+ * Simple test function for the Compass Simulation module
+ * @return 1 if successful, otherwise 0
+ */
+static int testCompass(void)
+{
+    Compass_Sim_Config_t simConfig;
+    Compass_Config_t compassConfig;
+    double compassValue;
+
+    /*
+     * Local file dataStore.bin - this should work regardless of platform
+     */
+    simConfig.path = "dataStore.bin";
+
+    /*
+     * This will be how the final API will be used.
+     */
+    compassConfig.measMode = Compass_Mode_Continuous;
+
+    if(!Compass_Sim_Init(&simConfig))
+    {
+        printf("testCompass: Failed to init simulator\n");
+        return 0;
+    }
+
+    if(!Compass_Init(&compassConfig))
+    {
+        printf("testCompass: Failed to init module\n");
+        return 0;
+    }
+
+    compassValue = Compass_Read();
+    printf("Compass: %lf\n", compassValue);
+
+    Compass_Sim_Deinit();
+
+    return 1;
+
 }
