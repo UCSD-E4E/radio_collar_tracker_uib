9d49d6faf1d9fe715edcc1c607356bf0e800ea5f
DavidSalzmann <davidsalzmannschool@gmail.com>
2021-04-12 16:22:17 -0700

    Forgot a ;

--- src/I2C.c
+++ src/I2C.c
@@ -160,7 +160,7 @@ int I2C_MasterTransmit(uint8_t deviceAddress, uint8_t* pData, uint16_t size, uin
  */
 int I2C_MasterRegisterTransmit(uint8_t deviceAddress, uint8_t registerAddress, uint8_t* pData, uint16_t size, uint32_t timeout_ms){
     
-    TW_Stop() //for testing
+    TW_Stop(); //for testing
 
     uint16_t i;
 
