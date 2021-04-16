6455338d3c85290b6e2cf47b20d3acd6cfec1a41
DavidSalzmann <davidsalzmannschool@gmail.com>
2021-04-15 17:29:16 -0700

    temp changes in uib

--- src/uib.c
+++ src/uib.c
@@ -94,18 +94,18 @@ void appMain(void)
       data_size = 0x03;
       read_check = I2C_MasterRegisterReceive(address, register_address, data_ptr, data_size, timeout_ms);
       Serial_Printf(HAL_SystemDesc.pOBC, "Press 'a' to continue \n\r");
-      while(user_input != 'a'){
-          Serial_Read(HAL_SystemDesc.pOBC, &user_input, sizeof(user_input));
-      }
-      user_input = 'b';
+    //   while(user_input != 'a'){
+    //       Serial_Read(HAL_SystemDesc.pOBC, &user_input, sizeof(user_input));
+    //   }
+    //   user_input = 'b';
 
-      //read_check = I2C_MasterRegisterReceivePt1(address, register_address, data_ptr, data_size, timeout_ms);
-      Serial_Printf(HAL_SystemDesc.pOBC, "Press 'a' to continue \n\r");
-      while(user_input != 'a'){
-          Serial_Read(HAL_SystemDesc.pOBC, &user_input, sizeof(user_input));
-      }
-      user_input = 'b';
-      TW_Stop();
+    //   //read_check = I2C_MasterRegisterReceivePt1(address, register_address, data_ptr, data_size, timeout_ms);
+    //   Serial_Printf(HAL_SystemDesc.pOBC, "Press 'a' to continue \n\r");
+    //   while(user_input != 'a'){
+    //       Serial_Read(HAL_SystemDesc.pOBC, &user_input, sizeof(user_input));
+    //   }
+    //   user_input = 'b';
+    //   TW_Stop();
     
 
         // nchars = Serial_Read(HAL_SystemDesc.pGPS, rxBuf, 63);
