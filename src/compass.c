8b5a67ecf42efc6d81f1849582bfe6e29f4f4025
DavidSalzmann <davidsalzmannschool@gmail.com>
2021-05-24 16:30:06 -0700

    Compass read and init functions created

--- src/compass.c
+++ src/compass.c
@@ -6,7 +6,8 @@
  */
 
 #include "compass.h"
-
+#include "I2C.h"
+#include <math.h>
 
 /**
  * Initializes the Compass module
@@ -16,8 +17,23 @@
  */
 int Compass_Init(Compass_Config_t* pConfig)
 {
-//    TODO
-    return 1;
+    uint8_t address = 0x1E;
+    uint8_t register_address = 0x00;
+    uint8_t data[3] = {};
+    uint8_t data_size = 0x03;
+    uint32_t timeout_ms = 0x00; //not yet implemented
+
+    //Set up CRA with TS enabled, 8 averaging, Data output to 30Hz
+    register_address = 0x00;
+    data[0] = 0xF4;
+    data[1] = 0x20; 
+    data[2] = pConfig->measMode; //sends the given measurement mode to the Mode Register on the compass
+
+    if(I2C_MasterRegisterTransmit(address, register_address, data, data_size, timeout_ms) == 1){ //with *data_ptr being 0x00, this will tell the compass to address it's 0th   
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
 /**
@@ -29,6 +45,26 @@ int Compass_Init(Compass_Config_t* pConfig)
  */
 int Compass_Read(int16_t *pValue)
 {
-    // TODO
+    uint8_t address = 0x1E;
+    uint8_t register_address = 0x03;
+    uint8_t data[6] = {};
+    uint8_t data_size = 0x06;
+    uint32_t timeout_ms = 0x00; //not yet implemented
+
+    signed int16_t dataX = 0x00;
+    signed int16_t dataY = 0x00;
+    signed int16_t dataZ = 0x00;
+
+    float headerRad = 0x00;
+    
+    read_check = I2C_MasterRegisterReceive(address, register_address, data, data_size, timeout_ms);
+
+    dataX = (data[0]) + (data[1] << 8); //String together the MSB and LSB of data for each axis
+    dataZ = (data[2]) + (data[3] << 8); 
+    dataY = (data[4]) + (data[5] << 8);
+
+    headerRad = atan2((float)dataY, (float)dataX); //find the distance from 
+    *pValue = (uint16_t)(headerRad*360/(2*PI));
+
     return 1;
 }
