1771843758e84e4d3443ffeb8909cdec6d262bbb
DavidSalzmann <davidsalzmannschool@gmail.com>
2021-07-03 18:22:28 -0700

    Minor Bug Fixes

--- src/timerfile.c
+++ src/timerfile.c
@@ -15,9 +15,9 @@ uint32_t count3;
 int Timer3Innit(){
     
     sei();
-    SETMASK((1 << CS30) | (1 << WGM32), TCCR3B); //Set CS12:0 to 1 so that Timer 1's clock is ClkIO, or 1Mhz
+    SETMASK((1 << CS30) | (1 << WGM32), TCCR3B); //Set CS32:0 to 1 so that Timer 3's clock is ClkIO, or 1Mhz
     CLEARMASK((1 << WGM30) | (1 << WGM31), TCCR3A); //Set WGM13:0 to 4 for Continuos Measurement Mode CTC
-    CLEARMASK((1 << WGM33), TCCR3B); 
+    CLEARMASK((1 << WGM33) | (1 << CS31) | (1 << CS32), TCCR3B); 
     SETMASK((1 << OCIE3A), TIMSK3); //Set Interrupt enable of Timer 1's output compare register
     OCR3AH = 0x01; 
     OCR3AL = 0xF3; //Set the top value of the Output Compare Register A for Timer 0 to be 499, or 0x01F3
