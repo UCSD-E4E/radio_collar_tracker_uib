68e54824d190504aef9adbf2adca09cd131ecbb7
DavidSalzmann <davidsalzmannschool@gmail.com>
2021-07-03 17:58:39 -0700

    Removed timer.c and timer.h, updated timerfile to use timer1

--- src/timerfile.c
+++ src/timerfile.c
@@ -15,11 +15,12 @@ uint32_t count0;
 int Timer0Innit(){
     
     sei();
-    SETMASK((1 << CS01) | (1 << WGM02), TCCR0B); //Set CS0:2 to 2 so that Timer 0's clock is ClkIO/8, or 125kHz
-    SETMASK((1 << WGM00) | (1 << WGM01), TCCR0A); 
-    SETMASK((1 << OCIE0A), TIMSK0); //Set Interrupt enable of Timer 0's output compare register
-    OCR0A = 0x7C; //Set the top value of the Output Compare Register A for Timer 0 to be 124
-
+    SETMASK((1 << CS10) | (1 << WGM12), TCCR1B); //Set CS12:0 to 1 so that Timer 1's clock is ClkIO, or 1Mhz
+    CLEARMASK((1 << WGM10) | (1 << WGM11), TCCR1A); //Set WGM13:0 to 4 for Continuos Measurement Mode CTC
+    CLEARMASK((1 << WGM13), TCCR1B); 
+    SETMASK((1 << OCIE1A), TIMSK1); //Set Interrupt enable of Timer 1's output compare register
+    OCR1AH = 0x01; 
+    OCR1AH = 0xF3; //Set the top value of the Output Compare Register A for Timer 0 to be 499, or 0x01F3
     count0 = 0x00000000; //reset Timer0's counter
     return 1;
 
@@ -27,13 +28,13 @@ int Timer0Innit(){
 
 int GetTimer0(){
     uint32_t current_time;
-    CLEARMASK((1 << OCIE0A), TIMSK0);
+    CLEARMASK((1 << OCIE1A), TIMSK1);
     current_time = count0;
-    SETMASK((1 << OCIE0A), TIMSK0);
+    SETMASK((1 << OCIE1A), TIMSK1);
     return current_time;
 
 }
 
-ISR(TIMER0_COMPA_vect){
+ISR(TIMER1_COMPA_vect){
     count0++;
 }
--- src/uib.c
+++ src/uib.c
@@ -91,7 +91,7 @@ void appMain(void)
     while(1)
     {
         timer_value = GetTimer0();
-        Serial_Printf(HAL_SystemDesc.pOBC, "Timer Value: %"PRIu32"\n\r", timer_value);
+        Serial_Printf(HAL_SystemDesc.pOBC, "Timer Value: %zu\n\r", timer_value);
         //DRDY Check on the compass
         //register_address = 0x09;
         //data_size = 0x01;
